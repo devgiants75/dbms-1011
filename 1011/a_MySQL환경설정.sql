c9c2db8aaaa575bfa6aef83b63f1b94d5ba19804
devgiants75 <devgiants75@naver.com>
2023-10-11 20:00:41 +0900

    20231011

---
+++ 1011/a_MySQL환경설정.sql
@@ -0,0 +1,41 @@
+### DBMS 환경설정 ###
+
+# 윈도우 키 or 검색창 - 프로그램 추가/제거
+# 검색창에 'mysql' 검색
+# 설치되어 있는 앱 삭제 
+
+### MySQL 설치 ###
+
+# 1. 구글 - mysql 검색
+
+# 2. MySQL 공식 사이트
+# : MySQL 홈페이지 접속 - 상단 메뉴바 - Downloads 클릭
+
+# 3. 다운로드 화면
+# : 화면 스크롤 - 가장 아래에 있는 MySQL Community (GPL) Downloads 클릭
+# : 다음 화면에 표시된 화면의 리스트에 있는 MySQL Installer for Windows 클릭
+
+# 4. 인스톨러 다운로드 화면
+# : Windows (x86, 32-bit), MSI Installer 두 개 중
+# : 아래 쪽의 Download를 클릭
+
+# 5. 로그인/등록 없이 다운로드 시작
+# : No thanks, just start my downloads버튼 클릭
+
+# 6. 인스톨러 실행
+# : 다운로드 한 인스톨러 파일을 파일 탐색기에서 확인하여 더블 클릭 실행
+
+### MySQL Installer ###
+
+# 1. 설치 타입 설정
+# [Choosing a Setup Type]에서는 설치 유형을 선택
+# Custom 클릭 후 Next
+# : 필요한 것들만 골라서 설치 가능
+
+# 2. [Select Products and Features] 
+# : 설치할 제품들을 선택
+# : [MySQL Servers] – [MySQL Server] – [MySQL Server 8.0] 
+
+# Config Type: Development Computer
+# 3306, 3308
+# root
