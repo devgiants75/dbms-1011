dc2ac50dc8da5f0356e57eeba64aaa10370683fe
devgiants75 <devgiants75@naver.com>
2023-10-11 21:40:44 +0900

    20231011

---
+++ 1011/d_SQL명령어종류.sql
@@ -0,0 +1,20 @@
+# SQL 명령어 종류 #
+# MySQL 기초 문법 #
+
+-- SQL에서 DBMS에 실행 할 수 있는 명령의 종류에 따라
+-- DDL, DML, DCL 3가지로 분류
+
+-- 1. DDL (Data Definition Language)
+-- : 데이터 정의 언어
+-- : 테이블의 구조나 관계를 생성하는데 사용
+-- : create, drop, truncate, alter
+
+-- 2. DML (Data Manipulation Language)
+-- : 데이터 조작 언어
+-- : 테이블에 데이터 검색, 등록, 수정, 삭제하는데 사용
+-- : select, update, delete, insert
+
+-- 3. DCL (Data Control Language)
+-- : 데이터 제어 언어
+-- : 데이터의 보안, 무결성, 회복 등을 정의하는데 사용
+-- : grant, revoke 
