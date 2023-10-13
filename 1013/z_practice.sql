c8b0487e122a00a0cdd987b563f1d78d66cc1097
devgiants75 <devgiants75@naver.com>
2023-10-13 21:45:17 +0900

    20231013

--- 1013/z_practice.sql
+++ 1013/z_practice.sql
@@ -5,7 +5,8 @@
 
 /*
 테이블 설계
-Students
+: 테이블명(Students)
+
 StudentID: 학생을 유일하게 식별하는 ID.
 FirstName: 학생의 이름.
 LastName: 학생의 성.
@@ -16,6 +17,7 @@ Email: 학생의 이메일.
 
 /*
 데이터 타입 및 제약조건 선택
+
 StudentID: INT, PRIMARY KEY, NOT NULL
 FirstName: VARCHAR(50), NOT NULL
 LastName: VARCHAR(50), NOT NULL
@@ -23,3 +25,20 @@ Age: INT, CHECK(Age >= 18 AND Age <= 100)
 Major: VARCHAR(100)
 Email: VARCHAR(100), UNIQUE
 */
+
+/*
+열 추가/수정/삭제
+
+Students 테이블에 Gender 칼럼을 추가합니다.
+- 데이터타입 VARCHAR(10)
+
+Students 테이블의 Age 칼럼 데이터 타입을 수정합니다.
+- 데이터타입 SMALLINT로 수정
+*/
+
+/*
+테이블 삭제
+
+스키마 삭제
+*/
+ 
