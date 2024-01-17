f6f01d074771462cdf07fdb66b711ec11022fd1b
devgiants75 <devgiants75@naver.com>
2024-01-17 14:06:12 +0900

    20240117

---
+++ 1107/f_데이터베이스예시.sql
@@ -0,0 +1,7 @@
+### 데이터베이스 구조 예제 ###
+
+# MySQL Sample Database1
+# https://www.mysqltutorial.org/mysql-sample-database.aspx
+
+# MySQL Sample Database2
+# https://www3.ntu.edu.sg/home/ehchua/programming/sql/SampleDatabases.html
